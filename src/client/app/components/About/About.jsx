bccd1e5b193a0276089297995aa0db553d66e773
Nick DeJesus <ndejesus@sapient.com>
2016-10-09 11:55:32 -0400

    adding more styling and content

--- src/client/app/components/About/About.jsx
+++ src/client/app/components/About/About.jsx
@@ -4,8 +4,18 @@ import { Link } from 'react-router';
 class About extends React.Component {
 	render () {
 		return (
-			<div className="about-container">
-				About Page
+			<div className="about-container row">
+			<div className="page-section large-6 large-centered">
+				<h2>About Us</h2>
+				<p>
+					Remote collaboration is still very much a big challenge for many teams.
+					We want to be able to virtualize a team in different geography to replicate
+					the experience of being in the same room together. We are a team of veteran
+					entrepreneurs who have had successes building products. We have collectively
+					started 4 companies, with 2 acquired and 1 taken public. We are passionate 
+					about simple, elegant modes of communication and the impact they can have.
+				</p>
+			</div>
 			</div>
 			)
 	}
--- src/client/app/components/Home/Home.jsx
+++ src/client/app/components/Home/Home.jsx
@@ -5,7 +5,21 @@ class Home extends React.Component {
 
     return (
     	<div className="main-container row">
-    	<h1>Welcome To Cyclops</h1>
+    		<div className='welcome large-6 large-centered columns'>
+    			<h1>Welcome To Cyclops</h1>
+    			<p>Brief Description of what Cyclops is/does</p>
+    		</div>
+    		<div className="large-6 large-centered page-section video">
+    			<h2>Nate's Video</h2>
+    		</div>
+    		<div className="large-6 large-centered page-section demo">
+    			<h3>Demo Of Product</h3>
+    			<p>
+    				There were lots of ideas around this. Live streamed video can be used here
+    				or pre-recorded video stream with options to use Cyclop features on it. 
+    				Waikit was thinking of giving people the option to create a room immediately.
+    			</p>
+    		</div>
     	</div>
     	)
   }
